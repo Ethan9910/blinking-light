a929a2dc67e0df39c7eb2ff9e87377e27bb75aec
Ethan9910 <44172842+Ethan9910@users.noreply.github.com>
2018-10-15 12:53:30 -0400

    Add files via upload

---
+++ light_pattern/light_pattern.ino
@@ -0,0 +1,51 @@
+int x=50;
+void setup() {pinMode (3,OUTPUT);
+pinMode (4,OUTPUT);
+pinMode (5,OUTPUT);
+pinMode (6,OUTPUT);
+pinMode (7,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {digitalWrite(3,HIGH);
+delay(x);
+digitalWrite(3,LOW);
+delay(x);
+digitalWrite(4,HIGH);
+delay(x);
+digitalWrite(4,LOW);
+delay(x);
+digitalWrite(5,HIGH);
+delay(x);
+digitalWrite(5,LOW);
+delay(x);
+digitalWrite(6,HIGH);
+delay(x);
+digitalWrite(6,LOW);
+delay(x);
+digitalWrite(7,HIGH);
+delay(x);
+digitalWrite(7,LOW);
+delay(x);
+digitalWrite(7,HIGH);
+delay(x);
+digitalWrite(7,LOW);
+delay(x);
+digitalWrite(6,HIGH);
+delay(x);
+digitalWrite(6,LOW);
+delay(x);
+digitalWrite(5,HIGH);
+delay(x);
+digitalWrite(5,LOW);
+delay(x);
+digitalWrite(4,HIGH);
+delay(x);
+digitalWrite(4,LOW);
+delay(x);
+digitalWrite(3,HIGH);
+delay(x);
+digitalWrite(3,LOW);
+delay(x);
+}
